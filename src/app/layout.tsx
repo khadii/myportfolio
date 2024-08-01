73e7c454b94a4f89e6cd89ae53da52505b354a52
Your GitHub Username <Your GitHub Email>
2024-08-01 08:21:46 -0600

    Added changes

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <div className="bg-[#222] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden no-scrollbar">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
